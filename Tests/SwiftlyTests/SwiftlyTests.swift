623c8c9372b161dc26c37770c78c13ec86fa0c97
MariuszJakowienko <mariusz.jakowienko@miquido.com>
2020-09-16 20:05:14 +0200

    Added test for removing images from cache.

--- Tests/SwiftlyTests/SwiftlyTests.swift
+++ Tests/SwiftlyTests/SwiftlyTests.swift
@@ -34,11 +34,36 @@ final class SwiftlyTests: XCTestCase {
     }
 
     func testRemoveImage() {
-
+        let expectedImage = UIColor.red.image(CGSize(width: 1024,
+                                             height: 1024))
+        let url = URL(string: "http://test/image.jpg")!
+        let cache = ImageCache(config: .defaultConfig)
+        cache.insertImage(expectedImage, for: url)
+        cache.removeImage(for: url)
+        let image = cache.imageCache.object(forKey: url as AnyObject) as? UIImage
+        XCTAssertNil(image)
     }
 
     func testRemoveAllImages() {
+        let firstExpectedImage = UIColor.red.image(CGSize(width: 1024,
+                                             height: 1024))
+        let secondExpectedImage = UIColor.yellow.image(CGSize(width: 1024,
+                                                              height: 1024))
+        let firstUrl = URL(string: "http://test/image.jpg")!
+        let secondUrl = URL(string: "http://test/image2.jpg")!
+
+
+        let cache = ImageCache(config: .defaultConfig)
+
+        cache.insertImage(firstExpectedImage, for: firstUrl)
+        cache.insertImage(secondExpectedImage, for: secondUrl)
+
+        cache.removeAllImages()
         
+        let firstImage = cache.imageCache.object(forKey: firstUrl as AnyObject) as? UIImage
+        let secondImage = cache.imageCache.object(forKey: secondUrl as AnyObject) as? UIImage
+        XCTAssertNil(firstImage)
+        XCTAssertNil(secondImage)
     }
 
     func testCacheImage() {
